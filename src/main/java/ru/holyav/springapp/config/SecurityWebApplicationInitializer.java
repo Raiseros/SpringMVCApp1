58cc5bffad177ee3981d1bd36537bb7f983873b9
Raiseros <holyavenko.91@mail.ru>
2020-03-02 21:11:35 +0300

    8

---
+++ src/main/java/ru/holyav/springapp/config/SecurityWebApplicationInitializer.java
@@ -0,0 +1,8 @@
+package ru.holyav.springapp.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityWebApplicationInitializer
+                        extends AbstractSecurityWebApplicationInitializer {
+
+}
