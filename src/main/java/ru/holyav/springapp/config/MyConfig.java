3ef93ade0d710adb404196710b4493a9e538f421
Raiseros <holyavenko.91@mail.ru>
2020-06-01 01:04:11 +0300

    updates for heroku2

--- src/main/java/ru/holyav/springapp/config/MyConfig.java
+++ src/main/java/ru/holyav/springapp/config/MyConfig.java
@@ -38,6 +38,23 @@ public class MyConfig {
         return new JdbcTemplate(dataSource());
     }
 
+     //DataSource for heroku
+    /*@Bean
+    public BasicDataSource dataSource() throws URISyntaxException {
+        URI dbUri = new URI(System.getenv("CLEARDB_DATABASE_URL"));
+
+        String username = dbUri.getUserInfo().split(":")[0];
+        String password = dbUri.getUserInfo().split(":")[1];
+        String dbUrl = "jdbc:mysql://" + dbUri.getHost() + dbUri.getPath();
+
+        BasicDataSource basicDataSource = new BasicDataSource();
+        basicDataSource.setUrl(dbUrl);
+        basicDataSource.setUsername(username);
+        basicDataSource.setPassword(password);
+
+        return basicDataSource;
+    }*/
+
 
     @Bean
     public DataSource dataSource() throws PropertyVetoException {
